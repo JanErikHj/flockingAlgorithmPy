14a4e45f7fff25034bfd72dad75532a92ad35e6c
Jan Erik Hjelseth <jan.erik.hjelseth@outlook.com>
2025-02-09 16:18:25 +0100

    Collision with vector reflect working with single entity, but failing with flock

--- simulation/entities.py
+++ simulation/entities.py
@@ -15,15 +15,13 @@ class Entity:
         self.velocity = pygame.Vector2(random.uniform(-3, 3), random.uniform(-3, 3))
 
     def collision_detect(self):
-        if self.position.x <= 0:
-            self.position.x = sim.SCREEN_WIDTH
-        elif self.position.x >= sim.SCREEN_WIDTH:
-            self.position.x = 1
-        if self.position.y <= 0:
-            self.position.y = sim.SCREEN_HEIGHT
-        elif self.position.y > sim.SCREEN_HEIGHT:
-            self.position.y = 1
+        if self.position.x < 0 or self.position.x > sim.SCREEN_WIDTH:
+            self.velocity.reflect_ip(self.velocity.project(pygame.Vector2(1, 0)))
 
+        if self.position.y < 0 or self.position.y > sim.SCREEN_HEIGHT:
+            self.velocity.reflect_ip(self.velocity.project(pygame.Vector2(0, 1)))
+
+        self.velocity.scale_to_length(sim.MAX_SPEED)
 
 
     def distance_to(self, other):
@@ -33,31 +31,32 @@ class Entity:
         if len(neighbours) == 0:
             self.collision_detect()
             self.position += self.velocity
+
             return
 
         cohesion = pygame.Vector2(0, 0)
         alignment = pygame.Vector2(0, 0)
         separation = pygame.Vector2(0, 0)
 
-
         for neighbour in neighbours:
             if neighbour == self:
                 continue
 
             distance = self.distance_to(neighbour)
-            cohesion += neighbour.position
-            alignment += neighbour.velocity
+            cohesion += neighbour.position * (1/distance)
+            alignment += neighbour.velocity * (1/distance)
 
 
             if distance < sim.SEPARATION_DISTANCE:
-                separation += neighbour.position
+                separation += neighbour.position * (1/distance)
 
-        self.velocity += (cohesion / len(neighbours)) * sim.COHESION_GAIN
-        self.velocity += (alignment / len(neighbours)) * sim.ALIGNMENT_GAIN
-        self.velocity -= (separation / len(neighbours)) * sim.SEPARATION_GAIN
+        self.velocity += (cohesion / len(neighbours)) #* sim.COHESION_GAIN
+        self.velocity += (alignment / len(neighbours)) #* sim.ALIGNMENT_GAIN
+        self.velocity -= (separation / len(neighbours)) #* sim.SEPARATION_GAIN
         self.velocity.scale_to_length(sim.MAX_SPEED)
         self.collision_detect()
         self.position += self.velocity
+
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), (int(self.position.x), int(self.position.y)), 3)
